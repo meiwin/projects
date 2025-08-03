dcd2cc242d91330390769d8b3b1f8a9907b0e554
Meiwin Fu <venansius.meiwin@gmail.com>
2025-08-04 02:23:57 +0800

    update for singapore bus

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://meiwin.github.io',
-  base: process.env.NODE_ENV === 'production' ? '/projects' : '/',
   vite: {
     plugins: [tailwindcss()]
   }
